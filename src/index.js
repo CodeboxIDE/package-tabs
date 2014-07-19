a8df34e4d73ba13c8d1263a1ff28b2522bbf81ab
Samy Pessé <samypesse@gmail.com>
2014-07-18 21:45:15 -0700

    Remove test view

--- src/index.js
+++ src/index.js
@@ -8,17 +8,6 @@ define([
     // Add tabs to grid
     codebox.app.grid.addView(manager);
 
-    // Add a view
-    manager.add(hr.View, {}, {
-        title: "Test"
-    });
-
-    manager.on("tabs:opennew", function() {
-        manager.add(hr.View, {}, {
-            title: "Test"
-        });
-    });
-
     // Render the tabs manager
     manager.render();
 
